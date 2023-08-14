5e67eb31072fca31b78f145ece14829f80d99266
kaleabbyh <git-commit-address>
2023-08-14 03:11:02 +0300

    3o-40

--- AdventureDB-Solutions/Adventure-Solutions.sql
+++ AdventureDB-Solutions/Adventure-Solutions.sql
@@ -292,21 +292,278 @@ order by ContactTypeID desc
 
 Select * from  Person.BusinessEntityContact
 Select * from Person.ContactType
-
-Select C.Name,C.ContactTypeID, B.BusinessEntityID
-from  Person.BusinessEntityContact B
-join Person.ContactType C
-on B.ContactTypeID=C.ContactTypeID
-where C.Name like '%Purchasing Manager%' 
+ 
 
 
 
-Select P.businessentityid,lastname ,firstname 
-from Person.Person P 
-					--where P.businessentityid>360 
-					--order by P.businessentityid
-where  P.BusinessEntityID in (Select B.BusinessEntityID
-								from  Person.BusinessEntityContact B
-								join Person.ContactType C
-								on B.ContactTypeID=C.ContactTypeID
-								where C.Name like '%Purchasing Manager%') 
+SELECT pp.BusinessEntityID, LastName, FirstName
+FROM Person.BusinessEntityContact AS pb 
+JOIN Person.ContactType AS pc
+ON pc.ContactTypeID = pb.ContactTypeID
+JOIN Person.Person AS pp
+ON pp.BusinessEntityID = pb.PersonID
+WHERE pc.Name = 'Purchasing Manager'
+ORDER BY LastName, FirstName;
+
+--==========================================================================
+--21. From the following tables write a query in SQL to retrieve the salesperson for each PostalCode who belongs to a territory and SalesYTD is not zero.
+ --Return row numbers of each group of PostalCode, last name, salesytd, postalcode column. Sort the salesytd of each postalcode group in descending order. 
+ --Shorts the postalcode in ascending order.
+
+select * from Sales.SalesPerson
+select * from Person.Person
+select * from Person.Address
+
+
+select  ROW_NUMBER() OVER (PARTITION BY  pa.PostalCode ORDER BY  SalesYTD DESC) AS [Row Number],
+		pp.LastName, ss.SalesYTD, pa.PostalCode
+		
+from Sales.SalesPerson ss
+join Person.Person pp
+on ss.BusinessEntityID=pp.BusinessEntityID
+join person.Address pa
+on pa.AddressID=ss.BusinessEntityID
+WHERE TerritoryID IS NOT NULL
+	  AND SalesYTD <> 0
+ORDER BY PostalCode;
+
+
+
+--==========================================================================
+--22. From the following table write a query in SQL to count the number of contacts for combination of each type and name.
+ --Filter the output for those who have 100 or more contacts. Return ContactTypeID and ContactTypeName and BusinessEntityContact.
+ --Sort the result set in descending order on number of contacts.
+
+select * from Person.BusinessEntityContact
+
+select * from  Person.ContactType
+
+select pb.contacttypeid,pc.Name ctypename,count(*) nocontacts
+from Person.BusinessEntityContact pb
+join Person.ContactType pc
+on pb.ContactTypeID=pc.ContactTypeID
+group by pb.ContactTypeID, pc.Name
+having( count(*) >=100)
+order by COUNT(*) desc
+
+
+--==========================================================================
+--23. From the following table write a query in SQL to retrieve the RateChangeDate, full name (first name, middle name and last name) and 
+--weekly salary (40 hours in a week) of employees. In the output the RateChangeDate should appears in date format. Sort the output in 
+--ascending order on NameInFull.
+
+select * from  HumanResources.EmployeePayHistory
+select * from  Person.Person
+
+select	CAST(HE.RateChangeDate as date ) AS FromDate,
+		--( ISNULL(pp.FirstName,'')+', '+ ISNULL(pp.MiddleName,'')+' '+ISNULL(pp.LastName,'')) nameinfull,
+		CONCAT(PP.LastName, ', ', PP.FirstName, ' ', PP.MiddleName) AS NameInFull,
+		(HE.Rate*40) salaryinaweek
+from HumanResources.EmployeePayHistory HE
+join Person.Person PP
+on HE.BusinessEntityID=pp.BusinessEntityID
+order by nameinfull
+
+--==========================================================================
+--24. From the following tables write a query in SQL to calculate and display the latest weekly salary of each employee.
+--Return RateChangeDate, full name (first name, middle name and last name) and weekly salary (40 hours in a week) of employees Sort the output
+--in ascending order on NameInFull.
+
+select * from  HumanResources.EmployeePayHistory
+select * from  Person.Person
+
+select	CAST(HE.RateChangeDate as date ) AS FromDate,
+		CONCAT(PP.LastName, ', ', PP.FirstName, ' ', PP.MiddleName) AS NameInFull,
+		(HE.Rate*40) salaryinaweek
+from HumanResources.EmployeePayHistory HE
+join Person.Person PP
+on HE.BusinessEntityID=pp.BusinessEntityID
+ WHERE HE.RateChangeDate = (SELECT MAX(RateChangeDate)
+                                FROM HumanResources.EmployeePayHistory 
+                                WHERE BusinessEntityID = HE.BusinessEntityID
+								)
+order by nameinfull
+
+
+--==========================================================================
+--25. From the following table write a query in SQL to find the sum, average, count, minimum, and maximum order quentity for those orders 
+--whose id are 43659 and 43664. Return SalesOrderID, ProductID, OrderQty, sum, average, count, max, and min order quantity.
+
+select * from  Sales.SalesOrderDetail
+
+select SalesOrderId,
+		productid,
+		orderqty,
+		 sum(orderqty) OVER (PARTITION BY  ss.SalesOrderId ) [Total Quantity],
+		 avg(orderqty)  OVER (PARTITION BY  ss.SalesOrderId ) [Avg Quantity],
+		 count(*)      OVER (PARTITION BY  ss.SalesOrderId ) [No of Orders],
+		 min(OrderQty) OVER (PARTITION BY  ss.SalesOrderId ) [Min Quantity],
+		 max(OrderQty) OVER (PARTITION BY  ss.SalesOrderId ) [Max Quantity]
+from Sales.SalesOrderDetail ss
+where ss.SalesOrderId in (43659 , 43664)
+
+
+SELECT SalesOrderID, ProductID, OrderQty
+    ,SUM(OrderQty) OVER win AS "Total Quantity"
+    ,AVG(OrderQty) OVER win AS "Avg Quantity"
+    ,COUNT(OrderQty) OVER win AS "No of Orders"
+    ,MIN(OrderQty) OVER win AS "Min Quantity"
+    ,MAX(OrderQty) OVER win AS "Max Quantity"
+FROM Sales.SalesOrderDetail
+WHERE SalesOrderID IN(43659,43664)
+WINDOW win AS (PARTITION BY SalesOrderID);
+
+--==========================================================================
+--26. From the following table write a query in SQL to find the sum, average, and number of order quantity for those orders whose 
+--ids are 43659 and 43664 and product id starting with '71'. Return SalesOrderID, OrderNumber,ProductID, OrderQty, sum, average, 
+--and number of order quantity.
+
+
+select * from  Sales.SalesOrderDetail
+
+
+SELECT SalesOrderID AS OrderNumber, ProductID,
+    OrderQty AS Quantity,
+    SUM(OrderQty) OVER (ORDER BY SalesOrderID, ProductID) AS Total,
+    AVG(OrderQty) OVER(PARTITION BY SalesOrderID ORDER BY SalesOrderID, ProductID) AS Avg,
+    COUNT(OrderQty) OVER(ORDER BY SalesOrderID, ProductID ROWS BETWEEN UNBOUNDED PRECEDING AND 1 FOLLOWING) AS Count
+FROM Sales.SalesOrderDetail SS
+WHERE SalesOrderID IN(43659,43664) and CAST(ProductID AS varchar(10)) LIKE '71%';
+
+--==========================================================================
+--27. From the following table write a query in SQL to retrieve the total cost of each salesorderID that exceeds 100000. Return SalesOrderID, total cost.
+SELECT SalesOrderID, SUM(orderqty*unitprice) AS OrderIDCost  
+FROM Sales.SalesOrderDetail  
+GROUP BY SalesOrderID  
+HAVING SUM(orderqty*unitprice) > 100000.00  
+ORDER BY SalesOrderID;
+
+--==========================================================================
+--28. From the following table write a query in SQL to retrieve products whose names start with 'Lock Washer'. 
+--Return product ID, and name and order the result set in ascending order on product ID column.
+ select productid,name 
+ from Production.Product
+ where Name like 'Lock Washer%'
+ order by ProductID
+
+ --==========================================================================
+ --29. Write a query in SQL to fetch rows from product table and order the result set on an unspecified column listprice.
+ --Return product ID, name, and color of the product.
+
+ SELECT ProductID, Name, isnull(Color,'') color
+FROM Production.Product  
+ORDER BY ListPrice asc;
+
+select * from Production.Product 
+
+--==========================================================================
+--30. From the following table write a query in SQL to retrieve records of employees. 
+--Order the output on year (default ascending order) of hiredate. Return BusinessEntityID, JobTitle, and HireDate.
+
+select businessentityid,jobtitle ,hiredate
+from HumanResources.Employee
+order by YEAR(hiredate)
+
+--==========================================================================
+--31. From the following table write a query in SQL to retrieve those persons whose last name begins with letter 'R'. Return lastname,
+ --and firstname and display the result in ascending order on firstname and descending order on lastname columns.
+SELECT LastName, FirstName 
+FROM Person.Person  
+WHERE LastName LIKE 'R%'  
+ORDER BY FirstName ASC, LastName DESC ;
+
+--==========================================================================
+--32. From the following table write a query in SQL to ordered the BusinessEntityID column descendingly when SalariedFlag set to 'true' 
+--and BusinessEntityID in ascending order when SalariedFlag set to 'false'. Return BusinessEntityID, SalariedFlag columns.
+SELECT BusinessEntityID, SalariedFlag  
+FROM HumanResources.Employee  
+ORDER BY  CASE  SalariedFlag WHEN 'false' THEN BusinessEntityID END asc,
+			CASE SalariedFlag WHEN 'true' THEN BusinessEntityID END DESC  
+       
+--==========================================================================
+--33. From the following table write a query in SQL to set the result in order by the column TerritoryName when 
+--the column CountryRegionName is equal to 'United States' and by CountryRegionName for all other rows.
+SELECT BusinessEntityID, LastName, TerritoryName, CountryRegionName  
+FROM Sales.vSalesPerson  
+WHERE TerritoryName IS NOT NULL  
+ORDER BY CASE CountryRegionName WHEN 'United States' THEN TerritoryName  
+         ELSE CountryRegionName END;
+
+
+--==========================================================================
+--34. From the following table write a query in SQL to find those persons who lives in a territory and the value of salesytd except 0. 
+--Return first name, last name,row number as 'Row Number', 'Rank', 'Dense Rank' and NTILE as 'Quartile', salesytd and postalcode. 
+--Order the output on postalcode column.
+SELECT p.FirstName, p.LastName  
+    ,ROW_NUMBER() OVER (ORDER BY a.PostalCode) AS "Row Number"  
+    ,RANK() OVER (ORDER BY a.PostalCode) AS "Rank"  
+    ,DENSE_RANK() OVER (ORDER BY a.PostalCode) AS "Dense Rank"  
+    ,NTILE(4) OVER (ORDER BY a.PostalCode) AS "Quartile"  
+    ,s.SalesYTD, a.PostalCode  
+FROM Sales.SalesPerson AS s   
+    INNER JOIN Person.Person AS p   
+        ON s.BusinessEntityID = p.BusinessEntityID  
+    INNER JOIN Person.Address AS a   
+        ON a.AddressID = p.BusinessEntityID  
+WHERE TerritoryID IS NOT NULL AND SalesYTD <> 0;
+
+
+--==========================================================================
+--From the following table write a query in SQL to skip the first 10 rows from the sorted result set and return all remaining rows.
+
+SELECT DepartmentID, Name, GroupName  
+FROM HumanResources.Department  
+ORDER BY DepartmentID OFFSET 10 ROWS;
+
+--==========================================================================
+--36. From the following table write a query in SQL to skip the first 5 rows and return the next 5 rows from the sorted result set.
+SELECT DepartmentID, Name, GroupName  
+FROM HumanResources.Department  
+ORDER BY DepartmentID   
+    OFFSET 5 ROWS  
+    FETCH NEXT 5 ROWS ONLY;
+
+--==========================================================================
+--37. From the following table write a query in SQL to list all the products that are Red or Blue in color. Return name, color 
+--and listprice.Sorts this result by the column listprice.
+
+SELECT Name, Color, ListPrice  
+FROM Production.Product  
+WHERE Color = 'Blue'  
+UNION ALL  
+SELECT Name, Color, ListPrice  
+FROM Production.Product  
+WHERE Color = 'Red'  
+
+
+ORDER BY ListPrice ASC;
+
+
+--==========================================================================
+--38. Create a SQL query from the SalesOrderDetail table to retrieve the product name and any associated sales orders.
+ --Additionally, it returns any sales orders that don't have any items mentioned in the Product table as well as any products that 
+ --have sales orders other than those that are listed there. Return product name, salesorderid. Sort the result set on product name column.
+
+SELECT pp.Name, SS.SalesOrderID  
+FROM Production.Product AS pp
+FULL OUTER JOIN Sales.SalesOrderDetail AS SS  
+ON pp.ProductID = SS.ProductID  
+ORDER BY pp.Name ;
+
+--==========================================================================
+--39. From the following table write a SQL query to retrieve the product name and salesorderid. 
+--Both ordered and unordered products are included in the result set.
+SELECT pp.Name, ss.SalesOrderID  
+FROM Production.Product AS pp
+LEFT OUTER JOIN Sales.SalesOrderDetail AS ss  
+ON pp.ProductID = ss.ProductID  
+ORDER BY pp.Name ;
+
+
+--==========================================================================
+--40. From the following tables write a SQL query to get all product names and sales order IDs. Order the result set on product name column.
+SELECT pp.Name, ss.SalesOrderID  
+FROM Production.Product AS pp 
+INNER JOIN Sales.SalesOrderDetail AS ss  
+ON pp.ProductID = ss.ProductID  
+ORDER BY pp.Name ;
